e7c365e24bfad73ca15ccff092ac25e34a381a11
Boxuan Li <liboxuan@connect.hku.hk>
2018-12-04 13:53:20 +0800

    Add argparser

--- codebench/main.py
+++ codebench/main.py
@@ -1,2 +1,8 @@
+from codebench.parsing.DefaultArgParser import default_arg_parser
+
+
 def main():
-    print('hello world')
+    args = default_arg_parser().parse_args()
+    start_script = args.start
+    before_script = args.before
+    print(start_script, before_script)
---
+++ codebench/parsing/DefaultArgParser.py
@@ -0,0 +1,45 @@
+import argparse
+import os
+
+
+class PathArg(str):
+    """
+    Uni(xi)fying OS-native directory separators in path arguments.
+
+    Removing the pain from interactively using coala in a Windows cmdline,
+    because backslashes are interpreted as escaping syntax and therefore
+    removed when arguments are turned into coala settings
+
+    >>> import os
+    >>> PathArg(os.path.join('path', 'with', 'separators'))
+    'path/with/separators'
+    """
+
+    def __new__(cls, path):
+        return str.__new__(cls, path.replace(os.path.sep, '/'))
+
+
+def default_arg_parser():
+    """
+    This function creates an ArgParser to parse command line arguments.
+    """
+    description = """
+Automated code benchmark solution.
+Empower developers with tools to trace and analyze project performances.
+"""
+
+    arg_parser = argparse.ArgumentParser(description=description)
+
+    before_group = arg_parser.add_argument_group('Before')
+
+    before_group.add_argument(
+        '-b', '--before', type=PathArg, nargs=1, metavar='FILE',
+        help='file to be run before benchmarking')
+
+    start_group = arg_parser.add_argument_group('Start')
+
+    start_group.add_argument(
+        '-s', '--start', type=PathArg, nargs=1, metavar='FILE',
+        help='benchmark script to be run')
+
+    return arg_parser
