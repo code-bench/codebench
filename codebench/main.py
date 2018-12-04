09bcdbd0ccfd403038c9c866c896f31e9960bc98
Boxuan Li <liboxuan@connect.hku.hk>
2018-12-04 15:01:31 +0800

    Add time difference report

--- codebench/main.py
+++ codebench/main.py
@@ -1,8 +1,22 @@
+import subprocess
+
 from codebench.parsing.DefaultArgParser import default_arg_parser
+from codebench.performance.TimeUtilities import (
+    get_time,
+    get_time_difference,
+)
 
 
 def main():
     args = default_arg_parser().parse_args()
-    start_script = args.start
-    before_script = args.before
-    print(start_script, before_script)
+
+    # run the preparation script
+    if args.before is not None:
+        subprocess.call(args.before)
+
+    start_time = get_time()
+    # execute the benchmark program
+    subprocess.call(args.start)
+    end_time = get_time()
+    time_diff = get_time_difference(start_time, end_time)
+    print('time difference =', time_diff)
---
+++ codebench/performance/TimeUtilities.py
@@ -0,0 +1,9 @@
+import time
+
+
+def get_time():
+    return time.time()
+
+
+def get_time_difference(old_time, new_time):
+    return new_time - old_time
