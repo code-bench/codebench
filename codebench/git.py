978285528942f85a0a11e696073bd8eecb7185cf
Boxuan Li <liboxuan@connect.hku.hk>
2018-12-04 15:02:23 +0800

    Add git baseline option
    Users can provide a git commit hash as the baseline for benchmarking

---
+++ codebench/git.py
@@ -0,0 +1,2 @@
+def checkout(hash):
+    pass
--- codebench/parsing/DefaultArgParser.py
+++ codebench/parsing/DefaultArgParser.py
@@ -40,6 +40,12 @@ Empower developers with tools to trace and analyze project performances.
 
     start_group.add_argument(
         '-s', '--start', type=PathArg, nargs=1, metavar='FILE',
-        help='benchmark script to be run')
+        required=True, help='benchmark script to be run')
+
+    baseline_group = arg_parser.add_argument_group('Baseline')
+
+    baseline_group.add_argument(
+        '-l', '--baseline', type=str, nargs=1,
+        help='Commit hash for the baseline')
 
     return arg_parser
