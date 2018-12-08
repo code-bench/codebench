8b7ec53a997a19656693cb23053689760074dbb7
Boxuan Li <liboxuan@connect.hku.hk>
2018-12-08 22:31:01 +0800

    Update setup.py & requirements.txt
    Remove version contraints on requirements.
    Add install_requires
    Change classifiers.

--- setup.py
+++ setup.py
@@ -18,8 +18,18 @@ setuptools.setup(
             'codebench = codebench.main:main'
         ],
     },
+    install_requires=[
+        "GitPython",
+        "psutil",
+        "pyecharts",
+    ],
     classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
