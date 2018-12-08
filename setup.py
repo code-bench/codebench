0f29c2800bde1e25bba0bfe9092af59021e13176
Boxuan Li <liboxuan@connect.hku.hk>
2018-12-08 23:41:24 +0800

    setup.py: Change version to 0.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='codebench',
-    version='0.1',
+    version='0.0.1',
     author='Boxuan Li',
     author_email='liboxuan@connect.hku.hk',
     description='Automated code benchmark solution',
