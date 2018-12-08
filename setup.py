054426887b1edb5de64b002aaf3f622eb5b5fcab
Boxuan Li <liboxuan@connect.hku.hk>
2018-12-08 22:40:48 +0800

    setup.py: Use single quote

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name='codebench',
     version='0.1',
-    author="Boxuan Li",
-    author_email="liboxuan@connect.hku.hk",
-    description="Automated code benchmark solution",
+    author='Boxuan Li',
+    author_email='liboxuan@connect.hku.hk',
+    description='Automated code benchmark solution',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/li-boxuan/codebench",
+    long_description_content_type='text/markdown',
+    url='https://github.com/li-boxuan/codebench',
     packages=setuptools.find_packages(exclude=['tests']),
     entry_points={
         'console_scripts': [
@@ -19,18 +19,18 @@ setuptools.setup(
         ],
     },
     install_requires=[
-        "GitPython",
-        "psutil",
-        "pyecharts",
+        'GitPython',
+        'psutil',
+        'pyecharts',
     ],
     classifiers=[
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
 )
